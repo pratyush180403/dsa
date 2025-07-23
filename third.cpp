d6d897a2786ac08960a40ccceacbc5046f79a41c
pratyush180403 <dubeypratyush9999@gmail.com>
2025-07-23 22:52:27 +0530

    REMOVING THE COMMNET

--- third.cpp
+++ third.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 class Solution {
 public:
-    // Function to reverse a string
+   
     void reverseString(vector<char>& s) {
         int start = 0, end = s.size()-1;
         
-        // Until the string is reversed
+        
         while(start < end) {
-            // Swap the characters at start and end
+            
             char ch = s[start];
             s[start] = s[end];
             s[end] = ch;
             
-            // Move the pointers towards the center
+           
             start++, end--;
         }
         
@@ -24,12 +24,12 @@ public:
 
 int main() {
     vector<char> str = {'h', 'e', 'l', 'l', 'o'};
+
     
     
-    // Creating an instance of Solution class
     Solution sol;
     
-    // Function call to reverse the string
+    
     sol.reverseString(str);
 
     for (char c : str) {
