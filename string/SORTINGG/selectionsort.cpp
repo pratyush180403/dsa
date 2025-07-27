bf3190aff7060a6bfe85f54e2e1fe82a6d41e929
pratyush180403 <dubeypratyush9999@gmail.com>
2025-07-27 11:49:42 +0530

    selectionsort

---
+++ string/SORTINGG/selectionsort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>   
+#include <algorithm> 
+using namespace std;
+
+void printarray (int arr[] , int n ){
+    for(int i = 0 ; i<n ; i++){
+        cout << arr[i] << " " ; 
+    }
+    cout << endl;
+}
+void selectionesort (int arr[] , int n){
+    for (int i = 0; i < n-1 ; i++ ){
+        int smallestIndex = i;
+        for (int j=i+1 ; j < n ; j++){
+            if (arr[j] < arr[smallestIndex]){
+                smallestIndex = j ;
+            }
+        }
+        swap(arr[i] , arr[smallestIndex]);
+
+    }
+}
+
+
+
+int main(){
+    int n = 5;
+    int arr[] = {3 ,5 ,7 ,9  ,2};
+    selectionesort(arr , n);
+    printarray(arr , n);
+    return 0;
+}
